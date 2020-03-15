dd17863e9784a88f526838b64f314c4635ab828e
Nikita Eshkeev <neshkeev@yandex.ru>
2020-03-15 23:47:48 +0300

    [Util] Extract common functions
    In order to keep the DRY principle the common functions have been
    extracted into separate files: util.sh and logger.sh
    
    Signed-off-by: Nikita Eshkeev <neshkeev@yandex.ru>

---
+++ logger.sh
@@ -0,0 +1,48 @@
+
+function info() {
+	local msg="$1"
+	shift
+	printf "[INFO]: $msg\n" "$@"
+}
+
+function info_blue() {
+	local msg="$1"
+	shift
+	printf "\033[34m[INFO]: $msg\n\033[0m" "$@"
+}
+
+function info_purple() {
+	local msg="$1"
+	shift
+	printf "\033[35m[INFO]: $msg\n\033[0m" "$@"
+}
+
+function info_aqua() {
+	local msg="$1"
+	shift
+	printf "\033[36m[INFO]: $msg\n\033[0m" "$@"
+}
+
+function info_gray() {
+	local msg="$1"
+	shift
+	printf "\033[37m[INFO]: $msg\n\033[0m" "$@"
+}
+
+function success() {
+	local msg="$1"
+	shift
+	printf "\033[32m[INFO]: $msg\n\033[0m" "$@"
+}
+
+function warn() {
+	local msg="$1"
+	shift
+	printf "\033[33m[WARNING]: $msg\n\033[0m" "$@" >&2
+}
+
+function error() {
+	local msg="$1"
+	shift
+	printf "\033[31m[ERROR]: $msg\n\033[0m" "$@" >&2
+}
---
+++ util.sh
@@ -0,0 +1,152 @@
+#! /usr/bin/env bash
+
+set +x
+
+ROOT_DIR="$(dirname "$0")"
+
+source "$ROOT_DIR"/logger.sh || exit 1
+
+function is_env_set() {
+	local var="$1"
+	[ -n "${!var}" ]
+}
+
+function get_img_dir() {
+	local img_dir="$1"
+	[ -n "$img_dir" ] && {
+		echo "$img_dir"
+		return 0
+	}
+
+	is_env_set "DS_IMG_DIR" && {
+		echo "${DS_IMG_DIR}"
+		return 0
+	}
+	is_env_set "DS_HOME" && {
+		echo "$DS_HOME"/vms
+		return 0
+	}
+
+	echo "$HOME"/vms
+}
+
+function get_ram_dir() {
+	local ram_dir="$1"
+
+	[ -n "$ram_dir" ] && {
+		echo "$ram_dir"
+		return 0
+	}
+
+	is_env_set "DS_RAM_DIR" && {
+		echo "${DS_IMG_DIR}"
+		return 0
+	}
+	is_env_set "DS_HOME" && {
+		echo "$DS_HOME"/ram
+		return 0
+	}
+
+	echo "$HOME"/vms/ram
+}
+
+function solution_to_clipborad() {
+	local message="$1"
+	[ -z "$message" ] && {
+		error "Solution is empty. Nothing has been added to X11's clipboard" >&2
+		return 1
+	}
+
+	which xclip >/dev/null 2>&1 && {
+		xclip -sel clip <<< "$message" &&
+		info_purple "The command to solve the problem above added to X11's clipboard"
+	}
+}
+
+function dir_exists() {
+	local dir_name="$1"
+	[ -d "$dir_name" ] || {
+		error 'The directory "%s" does not exists, please create one:\n\tmkdir -p %s \033[0m' \
+			"$dir_name" \
+			"$dir_name"
+
+		solution_to_clipborad "$(printf "mkdir -p %s" "$dir_name")"
+		return 1
+	}
+}
+
+function ensure_dirs() {
+	for dir in "$@";
+	do
+		dir_exists "$dir" || return 1
+		success 'The "%s" directory [FOUND]' "$dir"
+	done
+}
+
+function calc_available_ram() {
+	local percentage=$(("${1:-80}" / 10))
+	[ "$percentage" -lt 10 ] || {
+		error "Unable to allocate %d%% of RAM" "$((percentage * 10))"
+		return 1
+	}
+
+	local total_mem=$(grep MemTotal /proc/meminfo | awk '{print $2}')
+	[ -n "$total_mem" ] && {
+		local available_mem=$((total_mem * percentage / 10))
+		echo "$available_mem"K
+	}
+}
+
+function check_dir_of_fs() {
+	local dir="$1"
+	local fs="${2:-tmpfs}"
+
+	[ ! -d "$dir" ] && {
+		error 'No directory "%s" found.' "$dir"
+		solution_to_clipborad "$(printf "mkdir -p %s" "$dir")"
+		return 1
+	}
+
+	$(which df) --type="$fs" "$dir" >/dev/null 2>&1 || {
+		local available_mem=$(calc_available_ram)
+		error 'The "%s" directory is not mounted into "%s". Please mount it:\n\tsudo mount -t %s -o rw,nodev,suid,size=%s %s %s\n' \
+			"$dir" "$fs" "$fs" "$available_mem" "$fs" "$dir"
+
+		solution_to_clipborad "$(echo sudo mount -t "$fs" -o rw,nodev,suid,size="$available_mem" "$fs" "$dir")"
+		return 1
+	}
+
+	success 'The "%s" directory is mounted to "%s"' "$dir" "$fs"
+}
+
+function check_tools_installed() {
+	for tool in which df rsync systemd-nspawn sudo grep awk ;
+	do
+		which "$tool" >/dev/null 2>&1 || {
+			error '%s [NOT FOUND]' "$tool"
+			solution_to_clipborad "sudo pacman -S $tool"
+			return 1
+		}
+
+		success '%s [FOUND]' $(which "$tool")
+	done
+}
+
+function check_container_name() {
+	local container="$1"
+	[ -n "$container" ] || {
+		error 'No container name specified. Usage:\n\t%s CONTAINER_NAME [OPTIONS]\n\n' "$0"
+		return 1
+	}
+}
+
+function common_prechecks() {
+	local container="$1"
+	local img_dir="$2"
+	local ram_dir="$3"
+
+	check_tools_installed &&
+		check_container_name "$container" &&
+		ensure_dirs "$img_dir" "$ram_dir" &&
+		check_dir_of_fs "$ram_dir"
+}
